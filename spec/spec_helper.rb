47c3f92817065eadb0ffda7b63f4c36876243d0a
James Harton <james@resistor.io>
2014-12-01 14:15:39 +1300

    For some reasons there's no `color_enabled` method on RSpec config anymore.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,6 @@ Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 Dir[File.expand_path('../workers/**/*.rb', __FILE__)].each { |f| require f }
 
 RSpec.configure do |config|
-  config.color_enabled = true
   config.formatter = :documentation
   config.extend ContextHelpers
   config.include SidekiqHelpers, example_group: {
